e70b7a6bd59b146833c94c0c9f5f38e1515967e1
Margarita <margaritayarmolchuk@gmail.com>
2024-03-17 10:50:51 +0000

    fix: add promary key to chat_to_link_connection table

--- migrations/sql/init_connection_table.sql
+++ migrations/sql/init_connection_table.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS scrapper.public.chat_to_link_connection
     chat_id BIGINT NOT NULL,
     link_id BIGINT NOT NULL,
 
+    PRIMARY KEY (chat_id, link_id),
     FOREIGN KEY (chat_id) REFERENCES scrapper.public.chat (id),
     FOREIGN KEY (link_id) REFERENCES scrapper.public.link (id)
 );
