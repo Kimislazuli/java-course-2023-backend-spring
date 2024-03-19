a2518766e2e19146a2282e7f63f5f6428a4e7927
Margarita <margaritayarmolchuk@gmail.com>
2024-03-19 12:33:37 +0000

    fix: add new try-with-resources

--- scrapper/src/test/java/edu/java/scrapper/MigrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/MigrationTest.java
@@ -2,7 +2,7 @@ package edu.java.scrapper;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSetMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,8 +11,9 @@ public class MigrationTest extends IntegrationTest {
     @Test
     public void chatTableTest() throws SQLException {
         try (Connection connection = POSTGRES.createConnection("");
-             PreparedStatement sqlQuery = connection.prepareStatement("SELECT * FROM public.chat")) {
-            String actualResult = sqlQuery.executeQuery().getMetaData().getColumnName(1);
+             PreparedStatement sqlQuery = connection.prepareStatement("SELECT * FROM public.chat");
+             ResultSet result = sqlQuery.executeQuery()) {
+            String actualResult = result.getMetaData().getColumnName(1);
             assertThat(actualResult).isEqualTo("id");
         }
     }
@@ -20,27 +21,26 @@ public class MigrationTest extends IntegrationTest {
     @Test
     public void linkTableTest() throws SQLException {
         try (Connection connection = POSTGRES.createConnection("");
-             PreparedStatement sqlQuery = connection.prepareStatement("SELECT * FROM link")) {
-            ResultSetMetaData result = sqlQuery.executeQuery().getMetaData();
-            String firstColumn = result.getColumnName(1);
-            String secondColumn = result.getColumnName(2);
-            String thirdColumn = result.getColumnName(3);
-            String fourthColumn = result.getColumnName(4);
+             PreparedStatement sqlQuery = connection.prepareStatement("SELECT * FROM link");
+             ResultSet result = sqlQuery.executeQuery()) {
+            String firstColumn = result.getMetaData().getColumnName(1);
+            String secondColumn = result.getMetaData().getColumnName(2);
+            String thirdColumn = result.getMetaData().getColumnName(3);
+            String fourthColumn = result.getMetaData().getColumnName(4);
             assertThat(firstColumn).isEqualTo("id");
             assertThat(secondColumn).isEqualTo("url");
             assertThat(thirdColumn).isEqualTo("last_update");
             assertThat(fourthColumn).isEqualTo("last_check");
         }
-
     }
 
     @Test
     public void connectionTableTest() throws SQLException {
         try (Connection connection = POSTGRES.createConnection("");
-             PreparedStatement sqlQuery = connection.prepareStatement("SELECT * FROM chat_to_link_connection")) {
-            ResultSetMetaData result = sqlQuery.executeQuery().getMetaData();
-            String firstColumn = result.getColumnName(1);
-            String secondColumn = result.getColumnName(2);
+             PreparedStatement sqlQuery = connection.prepareStatement("SELECT * FROM chat_to_link_connection");
+             ResultSet result = sqlQuery.executeQuery()) {
+            String firstColumn = result.getMetaData().getColumnName(1);
+            String secondColumn = result.getMetaData().getColumnName(2);
             assertThat(firstColumn).isEqualTo("chat_id");
             assertThat(secondColumn).isEqualTo("link_id");
         }
