3064ca2aaad773a447998af9e943c80b6794e54c
Margarita <margaritayarmolchuk@gmail.com>
2024-03-19 08:39:43 +0000

    refactor: remove unused import

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -2,14 +2,11 @@ package edu.java.scrapper;
 
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import liquibase.Contexts;
 import liquibase.LabelExpression;
 import liquibase.Liquibase;
-import liquibase.database.Database;
-import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.DirectoryResourceAccessor;
