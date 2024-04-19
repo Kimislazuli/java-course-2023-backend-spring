acf8f1d2bda173d1b39eb5f1f20f00086a14f818
Margarita <margaritayarmolchuk@gmail.com>
2024-04-19 11:19:16 +0000

    fix: micrometer

--- bot/src/main/java/edu/java/bot/bot_logic/Bot.java
+++ bot/src/main/java/edu/java/bot/bot_logic/Bot.java
@@ -9,27 +9,24 @@ import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.request.SetMyCommands;
 import edu.java.bot.bot_logic.commands.Command;
 import edu.java.bot.configuration.ApplicationConfig;
+import io.micrometer.core.instrument.Counter;
 import jakarta.annotation.PostConstruct;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class Bot implements AutoCloseable, UpdatesListener {
     private final ApplicationConfig config;
     private TelegramBot telegramBot;
     private final MessageHandler messageHandler;
-
+    private final Counter processedMessagesCounter;
     private final List<? extends Command> commands;
 
-    @Autowired
-    public Bot(ApplicationConfig config, MessageHandler messageHandler, List<? extends Command> commands) {
-        this.config = config;
-        this.messageHandler = messageHandler;
-        this.commands = commands;
-    }
+
 
     @Override
     public int process(List<Update> updates) {
@@ -43,8 +40,10 @@ public class Bot implements AutoCloseable, UpdatesListener {
                     );
                 }
             }
+
         );
 
+        processedMessagesCounter.increment();
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
