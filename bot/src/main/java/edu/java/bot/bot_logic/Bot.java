0f1d5960b2c967dc96c867e0c6cd0795e3100cbc
Margarita <margaritayarmolchuk@gmail.com>
2024-04-21 11:03:18 +0000

    chore: checkstyle

--- bot/src/main/java/edu/java/bot/bot_logic/Bot.java
+++ bot/src/main/java/edu/java/bot/bot_logic/Bot.java
@@ -14,7 +14,6 @@ import jakarta.annotation.PostConstruct;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Slf4j
