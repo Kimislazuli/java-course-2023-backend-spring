b133e8d134126d5b2b0ae68f4aa3abb01b6afca2
Margarita <margaritayarmolchuk@gmail.com>
2024-04-23 14:37:32 +0000

    fix: bug with activity

--- scrapper/src/main/java/edu/java/scrapper/client/GithubClient.java
+++ scrapper/src/main/java/edu/java/scrapper/client/GithubClient.java
@@ -1,13 +1,10 @@
 package edu.java.scrapper.client;
 
 import edu.java.scrapper.dto.github.GithubActivityResponse;
-import edu.java.scrapper.dto.github.GithubResponse;
 import java.time.OffsetDateTime;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.util.retry.Retry;
 
@@ -19,23 +16,6 @@ public class GithubClient {
     public GithubClient(WebClient.Builder builder, String baseUrl, Retry retryBackoff) {
         this.retryBackoff = retryBackoff;
         webClient = builder.baseUrl(baseUrl).build();
-    }
-
-    public Optional<GithubResponse> fetchLastModificationTime(String user, String repository) {
-        String link = String.format("/repos/%s/%s/events", user, repository);
-        try {
-            return webClient
-                .get()
-                .uri(url -> url.path(link).queryParam("per_page", 1).build())
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<GithubResponse>>() {
-                })
-                .retryWhen(retryBackoff)
-                .blockOptional()
-                .map(List::getFirst);
-        } catch (Exception e) {
-            return Optional.empty();
-        }
     }
 
     public List<GithubActivityResponse> fetchActivity(String user, String repository, OffsetDateTime afterTime) {
--- scrapper/src/main/java/edu/java/scrapper/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scrapper/scheduler/LinkUpdaterScheduler.java
@@ -6,7 +6,6 @@ import edu.java.scrapper.client.GithubClient;
 import edu.java.scrapper.client.StackOverflowClient;
 import edu.java.scrapper.domain.model.link.Link;
 import edu.java.scrapper.dto.github.GithubActivityResponse;
-import edu.java.scrapper.dto.github.GithubResponse;
 import edu.java.scrapper.dto.stackoverflow.StackOverflowResponse;
 import edu.java.scrapper.exception.NotExistException;
 import edu.java.scrapper.service.processing_services.LinkService;
@@ -15,7 +14,6 @@ import edu.java.scrapper.service.sending_services.SenderService;
 import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -38,7 +36,7 @@ public class LinkUpdaterScheduler {
     @Scheduled(fixedDelayString = "${app.scheduler.interval}")
     public void update() throws NotExistException {
         log.info("Check updates");
-        List<Link> links = updaterService.findOldLinksToUpdate(OffsetDateTime.now().minus(Duration.ofMinutes(10)));
+        List<Link> links = updaterService.findOldLinksToUpdate(OffsetDateTime.now().minus(Duration.ofMinutes(5)));
         for (Link link : links) {
             updaterService.check(link.getId(), OffsetDateTime.now());
             if (link.getUrl().contains("github")) {
@@ -54,31 +52,25 @@ public class LinkUpdaterScheduler {
         String[] urlParts = link.getUrl().split("/");
         String owner = urlParts[urlParts.length - 2];
         String repo = urlParts[urlParts.length - 1];
-        Optional<GithubResponse> optionalGithubResponse = githubClient.fetchLastModificationTime(owner, repo);
-        if (optionalGithubResponse.isPresent()) {
-            GithubResponse githubResponse = optionalGithubResponse.get();
-            if (link.getLastUpdate().isBefore(githubResponse.lastModified())) {
-                List<GithubActivityResponse> activityResponses =
-                    githubClient.fetchActivity(owner, repo, githubResponse.lastModified());
-                for (GithubActivityResponse response : activityResponses) {
-                    if (response.activityType().equals(PUSH) || response.activityType().equals(PR)) {
-                        performTableUpdateAndTelegramNotification(
-                            link.getId(),
-                            link.getUrl(),
-                            githubResponse.lastModified(),
-                            response.activityType(),
-                            response.actor().login()
-                        );
-                    } else {
-                        performTableUpdateAndTelegramNotification(
-                            link.getId(),
-                            link.getUrl(),
-                            githubResponse.lastModified()
-                        );
-                    }
-                }
-
+        List<GithubActivityResponse> activityResponses =
+                githubClient.fetchActivity(owner, repo, link.getLastUpdate());
+        for (GithubActivityResponse response : activityResponses) {
+            if (response.activityType().equals(PUSH) || response.activityType().equals(PR)) {
+                performTableUpdateAndTelegramNotification(
+                        link.getId(),
+                        link.getUrl(),
+                        response.timestamp(),
+                        response.activityType(),
+                        response.actor().login()
+                );
+            } else {
+                performTableUpdateAndTelegramNotification(
+                        link.getId(),
+                        link.getUrl(),
+                        response.timestamp()
+                );
             }
+
         }
     }
 
@@ -88,28 +80,28 @@ public class LinkUpdaterScheduler {
         StackOverflowResponse stackOverflowResponse = stackOverflowClient.fetchLastModificationTime(question);
         if (link.getLastUpdate().isBefore(stackOverflowResponse.lastModified())) {
             performTableUpdateAndTelegramNotification(
-                link.getId(),
-                link.getUrl(),
-                stackOverflowResponse.lastModified()
+                    link.getId(),
+                    link.getUrl(),
+                    stackOverflowResponse.lastModified()
             );
         }
     }
 
     public void performTableUpdateAndTelegramNotification(long linkId, String url, OffsetDateTime updatedAt)
-        throws NotExistException {
+            throws NotExistException {
         updaterService.update(linkId, updatedAt);
         List<Long> linkedChatIds = linkService.linkedChatIds(linkId);
         service.send(new LinkUpdate(linkId, url, "", linkedChatIds, UpdateType.DEFAULT));
     }
 
     public void performTableUpdateAndTelegramNotification(
-        long linkId,
-        String url,
-        OffsetDateTime updatedAt,
-        String activityType,
-        String actorLogin
+            long linkId,
+            String url,
+            OffsetDateTime updatedAt,
+            String activityType,
+            String actorLogin
     )
-        throws NotExistException {
+            throws NotExistException {
         updaterService.update(linkId, updatedAt);
         List<Long> linkedChatIds = linkService.linkedChatIds(linkId);
         if (activityType.equals(PUSH)) {
